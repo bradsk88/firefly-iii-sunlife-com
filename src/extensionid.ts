54803aa5b742c8c87b2ed1539497a576613f5a01
Brad Johnson <bradsk88@gmail.com>
2023-02-20 23:00:27 -0600

    Update from upstream

--- src/extensionid.ts
+++ src/extensionid.ts
@@ -3,6 +3,6 @@ export const bankDomain = 'sunlife.com';
 export const autoRunStartURL = 'https://www.sunnet.sunlife.com/signin/mysunlife/home.wca';
 export const extensionPrimaryColorHex = 'febe10';
 export const extensionSecondaryColorHex = '003946';
-export const isSingleAccountBank = false; // TODO: If your bank only has one account (e.g. a credit card) set this true.
+export const isSingleAccountBank = true;
 export const extensionBankName = `SunLife RRSP`;
 export const hubExtensionId = 'kcghjdbnjdaabfigmmjgmgiaehjidpoh';
